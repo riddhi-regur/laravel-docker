8056252dd8d706c766fd952109c8ac3203ac9f6a
riddhi-regur <riddhi.jani@regur.net>
2026-04-02 16:55:40 +0530

    the local docker is running

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        host: "0.0.0.0", // Listen on all network interfaces
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: "localhost", // The browser connects via localhost
+        },
+    },
 });
